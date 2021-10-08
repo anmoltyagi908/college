687f82a72dbbf3cc9720c1cc7a4da655963ac0d6
Anmol TYAGI <Anmol Tyagi>
2021-10-08 08:01:26 +0530

    some changes

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,16 +5,16 @@ const Resume = ({ data }) => {
      <div className="row">
      <p>All Subject Syllabus Avilable.</p>
 
-     <a title="PAPA" href="https://www.linkedin.com/in/ram-lakhan-singh-61a924200">
+     <a title="PAPA" href="https://drive.google.com/file/d/1ZITDGmOsDFOPbQYIBUM8m3CfesjOOS04/view?usp=drivesdk">
           <h1>Database ManageMent System (DBMS).</h1>
               </a> </div>
               <div className="row">
-     <a title="PAPA" href="https://www.linkedin.com/in/ram-lakhan-singh-61a924200">
+     <a title="PAPA" href="https://drive.google.com/file/d/1ZGwmPzESNECXFnpMvQRMAeFgqh01UKqV/view?usp=drivesdk">
                <h1> Machine Learning Techniques (MLT).</h1>
               </a>
                    </div>
                    <div className="row">
-     <a title="PAPA" href="https://www.linkedin.com/in/ram-lakhan-singh-61a924200">
+     <a title="PAPA" href="https://drive.google.com/file/d/1ZVs124i1-zHHAk2k-xXP1YF6jgBthWnR/view?usp=drivesdk">
                 <h1>Design and Analysis of Algorithm (DAA).</h1>
               </a>
                    </div>
@@ -23,8 +23,8 @@ const Resume = ({ data }) => {
                <h1> Object Oriented System Design (OOSD).</h1>
               </a></div>
               <div className="row">
-     <a title="PAPA" href="https://www.linkedin.com/in/ram-lakhan-singh-61a924200">
-               <h1> Machine Learning Techniques (MLT).</h1>
+     <a title="PAPA" href="https://drive.google.com/file/d/1ZXSkah-khL_05QGX8PqQl_CU3QGW45PR/view?usp=drivesdk">
+               <h1>Compiler Design (CD).</h1>
               </a>
                    </div>
 
@@ -32,11 +32,27 @@ const Resume = ({ data }) => {
      <a title="PAPA" href="https://drive.google.com/file/d/1VI0mg80V3zmn8i1BKsUv1LmCpFqxnUxD/view?usp=drivesdk">
                <h1> Constitution of India (COI).</h1>
               </a>
-                   {/* </div> */}
-                   {/*  */}
+                  
                    </div>
+                   <section id="resume">
+<div className="row">
+   <p>All Subject Lab Syllabus Avilable.</p>
 
-    </section>
+<a title="" href="https://drive.google.com/file/d/1ZITDGmOsDFOPbQYIBUM8m3CfesjOOS04/view?usp=drivesdk">
+     <h1>Database ManageMent System Lab.</h1>
+         </a> </div>
+       
+              <div className="row">
+<a title="PAPA" href="https://drive.google.com/file/d/1ZKIjbZQZZcpMlDHLFTc9OTypEBtvrAAv/view?usp=drivesdk">
+           <h1>Design and Analysis of Algorithm Lab.</h1>
+         </a>
+              </div>
+              <div className="row">
+<a title="PAPA" href="https://drive.google.com/file/d/1ZJA05IamqZ4q1AWkdPh-Y7SMRl0Yltix/view?usp=drivesdk">
+          <h1> Compiler Design Lab.</h1>
+         </a></div>
+         </section>
+</section>
   );
   }
 
